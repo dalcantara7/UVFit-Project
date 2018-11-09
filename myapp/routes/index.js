e815142870ff890672ef54cfe597d87ee0f6a7c8
Evan Weiler <evanweiler@email.arizona.edu>
2018-11-09 14:29:23 -0700

    index?

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -5,14 +5,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', function (req, res) {
-  let name;
-
-  if (req.query.name) {
-    name = req.query.name;
-  } else {
-    name = 'anonymous';
-  }
-  res.send('Hi! ' + name);
+  res.send('../public/info.html');
 });
 
 module.exports = router;
