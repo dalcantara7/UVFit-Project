bccbc792dd041a725c947a31218d698c16e30f9c
EvanJWeiler <evanjweiler@gmail.com>
2018-11-08 21:38:56 -0700

    testing user route

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -1,14 +1,16 @@
-var express = require('express');
-var router = express.Router();
-router.get('/',function(req, res) {
-        var name;
-        if ( req.query.name) {
-                name = req.query.name;
-        } else {
-                name = "anonymous";
-        }
-        res.send("Hi! " + name);
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/', function (req, res) {
+  let name;
+
+  if (req.query.name) {
+    name = req.query.name;
+  } else {
+    name = 'anonymous';
+  }
+  res.send('Hi! ' + name);
 });
 
 module.exports = router;
-
--- myapp/routes/users.js
+++ myapp/routes/users.js
@@ -1,9 +1,32 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const mongoose = require('mongoose');
+
+const router = express.Router();
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  password: { type: String, required: true },
+});
+
+mongoose.connect('mongodb://localhost/mydb');
+
+const User = mongoose.model('User', userSchema);
+
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.post('/register', function (req, res, next) {
+  const currUser = new User({
+    name: req.query.name,
+    password: req.query.pass,
+  });
+
+  currUser.save(function (err, user) {
+    res.send(req.query.name + ' was successfully saved with id ' + user._id);
+  });
+});
+
+router.get('/', function (req, res, next) {
+  console.log('connected to users route');
+  res.send('test successful!');
 });
 
 module.exports = router;
