53e10b2f74a368f10127c3b5b3bd6df078ccd20c
EvanJWeiler <evanjweiler@gmail.com>
2018-11-08 23:50:58 -0700

    fixed hashing for real

--- myapp/routes/users.js
+++ myapp/routes/users.js
@@ -20,19 +20,18 @@ const User = mongoose.model('User', userSchema);
 /* GET users listing. */
 router.post('/register', function (req, res, next) {
   const passHash = bcrypt.hash(req.body.pass, null, null, function (err, hash) {
-    console.log('Hash: ' + hash);
-  });
-  const currUser = new User({
-    name: req.body.name,
-    password: passHash,
-  });
+    const currUser = new User({
+      name: req.body.name,
+      password: passHash,
+    });
 
-  currUser.save(function (err, currUser) {
-    if (!err) {
-      res.send(req.body.name + ' was successfully saved with id ' + currUser._id);
-    } else {
-      console.log(err);
-    }
+    currUser.save(function (err, currUser) {
+      if (!err) {
+        res.send(req.body.name + ' was successfully saved with id ' + currUser._id);
+      } else {
+        console.log(err);
+      }
+    });
   });
 });
 
