5a3317491b78ee88fb4824fcdbda9f3b310bbd21
Evan Weiler <evanweiler@email.arizona.edu>
2018-11-09 14:31:22 -0700

    index for real

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -5,7 +5,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', function (req, res) {
-  res.send('../public/info.html');
+  res.sendFile('public/index.html', { root: __dirname });
 });
 
 module.exports = router;
