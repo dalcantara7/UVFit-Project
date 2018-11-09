996f0cc40913b9d532f3822bb35e5716f7ece0bc
Evan Weiler <evanweiler@email.arizona.edu>
2018-11-09 15:11:49 -0700

    routing fix

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -5,7 +5,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', function (req, res) {
-  res.sendFile('public/index.html', { root: __dirname });
+  res.sendFile('../public/index.html');
 });
 
 module.exports = router;
