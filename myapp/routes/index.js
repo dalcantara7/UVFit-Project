e3ca319d337890ac1197e156b1e6980430db3356
Evan Weiler <evanweiler@email.arizona.edu>
2018-11-09 16:07:24 -0700

    test

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 router.get('/', function (req, res) {
   console.log(__dirname);
-  res.sendFile('../public/index.html', { root: __dirname });
+  res.sendFile('./public/index.html', { root: __dirname });
 });
 
 module.exports = router;
--- myapp/routes/test.js
+++ myapp/routes/test.js
@@ -5,7 +5,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', function (req, res) {
-  res.sendFile('../public/test.html', { root: __dirname });
+  res.sendFile('./public/test.html', { root: __dirname });
 });
 
 module.exports = router;
