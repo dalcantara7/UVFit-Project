17e040b7dd75ecb4791718e9d20af8f5b4b35c7b
EvanJWeiler <evanjweiler@gmail.com>
2018-11-09 00:05:47 -0700

    basic authentication

--- myapp/routes/users.js
+++ myapp/routes/users.js
@@ -9,7 +9,7 @@ const router = express.Router();
 router.use(bodyParser.urlencoded({ extended: false }));
 
 const userSchema = new mongoose.Schema({
-  name: { type: String, required: true },
+  username: { type: String, required: true },
   password: { type: String, required: true },
 });
 
@@ -18,25 +18,44 @@ mongoose.connect('mongodb://localhost/mydb');
 const User = mongoose.model('User', userSchema);
 
 /* GET users listing. */
-router.post('/register', function (req, res, next) {
+router.post('/register', function (req, res) {
   bcrypt.hash(req.body.pass, null, null, function (err, hash) {
     const currUser = new User({
-      name: req.body.name,
+      username: req.body.name,
       password: hash,
     });
 
     currUser.save(function (err, currUser) {
-      if (!err) {
-        res.send(req.body.name + ' was successfully saved with id ' + currUser._id);
-      } else {
-        console.log(err);
-      }
+      if (err) throw err;
+
+      res.send(req.body.name + ' was successfully saved with id ' + currUser._id);
     });
   });
 });
 
-router.get('/', function (req, res, next) {
-  res.send('test successful!');
+router.post('/auth', function (req, res, next) {
+  User.findOne({ username: req.bodyu.username }, function (err, user) {
+    if (err) throw err;
+
+    if (!user) {
+      res.status(401).json({ error: 'Bad username'});
+    } else {
+      bcrypt.compare(req.body.pass, user.password, function (err, valid) {
+        if (err) {
+          res.status(400).json({ error: err });
+        } else if (valid) {
+          res.send('Successful authentication!');
+        } else {
+          res.status(401).json({ error: 'Bad password' });
+        }
+      });
+    }
+  });
 });
+
+router.get('/', function (req, res) {
+  res.send('successfully accessed users route');
+});
+
 
 module.exports = router;
