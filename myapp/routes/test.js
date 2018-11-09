b44f03ef9ceec47ebacc255f27e0f5b464b9adfd
Evan Weiler <evanweiler@email.arizona.edu>
2018-11-09 16:20:20 -0700

    test

--- myapp/routes/test.js
+++ myapp/routes/test.js
@@ -1,12 +1,15 @@
 'use strict';
+var path = require('path');
 
 const express = require('express');
 
 const router = express.Router();
 
 router.get('/', function (req, res) {
-  console.log(__dirname + '/../public/test.html');
-  res.sendFile(__dirname + '/../public/test.html');
+  var filePath = path.resolve(__dirname + '/../public/test.html');
+
+  console.log(filePath);
+  res.sendFile(filePath);
 });
 
 module.exports = router;
