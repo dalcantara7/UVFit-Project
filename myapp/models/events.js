f502c34aee42dfc11f7cb772060d5ab50c15d8de
Evan Weiler <evanweiler@email.arizona.edu>
2018-11-10 11:51:59 -0700

    im bad

--- myapp/models/events.js
+++ myapp/models/events.js
@@ -1,7 +1,7 @@
 const db = require('../db');
 
 const eventSchema = new db.Schema({
-  longitde: { type: Number, required: true },
+  longitude: { type: Number, required: true },
   latitude: { type: Number, required: true },
   deviceID: { type: String, required: true },
   published_at: { type: Date, required: true, default: Date.now },
