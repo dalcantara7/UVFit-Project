976b9da4caa073552373f38bb05943ab778bf4c5
Evan Weiler <evanweiler@email.arizona.edu>
2018-11-10 12:08:15 -0700

    why

--- myapp/routes/devices.js
+++ myapp/routes/devices.js
@@ -20,7 +20,8 @@ router.post('/reportevent', function (req, res, next) {
   // if (!JSON.parse(req.body.data).longitude) { res.status(201).send('Missing longitude field'); }
   // if (!JSON.parse(req.body.data).latitude) { res.status(201).send('Missing latitude field'); }
   // if (!JSON.parse(req.body.data).deviceID) { res.status(201).send('Missing deviceID field'); }
-  console.log(typeof JSON.parse(req.body.data).longitude);
+  const type = typeof JSON.parse(req.body.data).longitude;
+  console.log(type);
 
   const currEvent = new Event({
     longitude: parseFloat(JSON.parse(req.body.data).longitude),
