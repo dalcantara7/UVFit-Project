d8fbc692010b8eab3204c3dfffdffa9ca82679bc
Evan Weiler <evanweiler@email.arizona.edu>
2018-11-23 00:27:31 -0700

    redirect .html

--- myapp/app.js
+++ myapp/app.js
@@ -38,7 +38,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
