624831fa98041b60abe611a830228c8b3b788da9
Evan Weiler <evanweiler@email.arizona.edu>
2018-11-22 20:39:08 -0700

    required -> require

--- myapp/models/events.js
+++ myapp/models/events.js
@@ -4,7 +4,7 @@ const eventSchema = new db.Schema({
   longitude: { type: Number, required: true },
   latitude: { type: Number, required: true },
   uvVal: { type: Number, required: true },
-  speed: { type: Number, require: true },
+  speed: { type: Number, required: true },
   deviceID: { type: String, required: true },
   published_at: { type: Date, required: true, default: Date.now },
 });
