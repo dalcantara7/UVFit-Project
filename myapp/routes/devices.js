b27b61861dc9798bf98f7d898735e100f94afa71
Evan Weiler <evanweiler@email.arizona.edu>
2018-12-06 14:28:47 -0700

    particle key

--- myapp/routes/devices.js
+++ myapp/routes/devices.js
@@ -158,7 +158,6 @@ router.get('/sendinfo', function (req, res, next) {
 
 router.post('/reportevent', function (req, res, next) {
   const data = JSON.parse(req.body.data);
-  console.log(data);
 
   // if (!data.hasOwnProperty('apiKey')) { res.status(400).json({ success: false, message: 'Missing device API key' }); }
   // if (!data.hasOwnProperty('longitude')) { res.status(400).json({ success: false, message: 'Missing longitude field' }); }
