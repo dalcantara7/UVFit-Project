edce2ad5850b540d0a5371130922833ec603309d
Evan Weiler <evanweiler@email.arizona.edu>
2018-12-06 17:51:50 -0700

    added dashboard

--- myapp/public/javascripts/userDashboard.js
+++ myapp/public/javascripts/userDashboard.js
@@ -32,7 +32,7 @@
         'Content-Type': 'application/json',
         x_auth: window.sessionStorage.getItem('token'),
       },
-      body: JSON.stringify(message);
+      body: JSON.stringify(message),
     };
 
     fetch(url, fetchOptions)
