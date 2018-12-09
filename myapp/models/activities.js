b4ef597ede1a3640870172e5f8018604b451d078
EvanJWeiler <evanjweiler@gmail.com>
2018-12-08 18:21:40 -0700

    activities model

--- myapp/models/activities.js
+++ myapp/models/activities.js
@@ -4,7 +4,7 @@ const Event = require('./events');
 const activitySchema = new db.Schema({
   startTime: { type: Date, required: true },
   published_at: { type: Date, default: Date.now },
-  events: [Event],
+  events: [{ type: Event }],
 });
 
 const Device = db.model('Activity', activitySchema);
