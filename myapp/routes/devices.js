aa8699efeb50c3a4f509b70d6aebcadc91d1b63d
EvanJWeiler <evanjweiler@gmail.com>
2018-12-09 00:36:08 -0700

    activity test

--- myapp/routes/devices.js
+++ myapp/routes/devices.js
@@ -164,7 +164,7 @@ router.post('/reportevent', function (req, res, next) {
   if (!data.hasOwnProperty('apiKey')) { res.status(400).json({ success: false, message: 'Missing device API key' }); }
   if (!data.hasOwnProperty('longitude')) { res.status(400).json({ success: false, message: 'Missing longitude field' }); }
   if (!data.hasOwnProperty('latitude')) { res.status(400).json({ success: false, message: 'Missing latitude field' }); }
-  //if (!req.body.hasOwnProperty('deviceID')) { res.status(400).json({ success: false, message: 'Missing deviceID field' }); }
+  if (!req.body.hasOwnProperty('deviceID')) { res.status(400).json({ success: false, message: 'Missing deviceID field' }); }
   if (!data.hasOwnProperty('uvVal')) { res.status(400).json({ success: false, message: 'Missing UV value field' }); }
   if (!data.hasOwnProperty('speed')) { res.status(400).json({ success: false, message: 'Missing speed field' }); }
   if (!data.hasOwnProperty('startTime')) { res.status(400).json({ success: false, message: 'Missing start time field' }); }
