8bef144ff3537405995886eda31f983c076fd0d3
EvanJWeiler <evanjweiler@gmail.com>
2018-12-08 18:32:42 -0700

    distance added to activity

--- myapp/models/activities.js
+++ myapp/models/activities.js
@@ -2,6 +2,7 @@ const db = require('../db');
 
 const activitySchema = new db.Schema({
   startTime: { type: Date, required: true },
+  distance: { type: Number, required: true },
   published_at: { type: Date, default: Date.now },
   events: [{ type: db.Schema.ObjectId, ref: 'Event' }],
 });
