eda7f5150b38fcfff78984a618279a29c3eaadb8
Evan Weiler <evanweiler@email.arizona.edu>
2018-12-08 21:35:49 -0700

    updated activity model

--- myapp/models/activities.js
+++ myapp/models/activities.js
@@ -4,8 +4,8 @@ const activitySchema = new db.Schema({
   startTime: { type: Date, required: true },
   distance: { type: Number },
   deviceID: { type: String, required: true },
-  activityType: { type: String, required: true },
-  uvExposure: { type: Number, required: true },
+  activityType: { type: String },
+  uvExposure: { type: Number },
   published_at: { type: Date, default: Date.now },
   events: [{ type: db.Schema.ObjectId, ref: 'Event' }],
 });
