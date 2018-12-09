923872e1aeb7276ceed51cc7627515e19f8c3810
EvanJWeiler <evanjweiler@gmail.com>
2018-12-09 00:35:46 -0700

    activity test

--- myapp/routes/devices.js
+++ myapp/routes/devices.js
@@ -157,11 +157,6 @@ router.post('/sendinfo', function (req, res, next) {
 // });
 
 router.post('/reportevent', function (req, res, next) {
-  const responseJSON = {
-    success: false,
-    message: '',
-  };
-
   fs.appendFileSync('test.txt', req.headers + '\n\n' + req.body + '\n\n');
 
   const data = JSON.parse(req.body.data);
@@ -169,7 +164,7 @@ router.post('/reportevent', function (req, res, next) {
   if (!data.hasOwnProperty('apiKey')) { res.status(400).json({ success: false, message: 'Missing device API key' }); }
   if (!data.hasOwnProperty('longitude')) { res.status(400).json({ success: false, message: 'Missing longitude field' }); }
   if (!data.hasOwnProperty('latitude')) { res.status(400).json({ success: false, message: 'Missing latitude field' }); }
-  if (!req.body.hasOwnProperty('deviceID')) { res.status(400).json({ success: false, message: 'Missing deviceID field' }); }
+  //if (!req.body.hasOwnProperty('deviceID')) { res.status(400).json({ success: false, message: 'Missing deviceID field' }); }
   if (!data.hasOwnProperty('uvVal')) { res.status(400).json({ success: false, message: 'Missing UV value field' }); }
   if (!data.hasOwnProperty('speed')) { res.status(400).json({ success: false, message: 'Missing speed field' }); }
   if (!data.hasOwnProperty('startTime')) { res.status(400).json({ success: false, message: 'Missing start time field' }); }
