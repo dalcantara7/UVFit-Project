ef0a134c22fd73ce6d84e59ff6a6af2737fa541c
EvanJWeiler <evanjweiler@gmail.com>
2018-12-09 01:03:37 -0700

    activity test

--- myapp/routes/devices.js
+++ myapp/routes/devices.js
@@ -157,9 +157,6 @@ router.post('/sendinfo', function (req, res, next) {
 // });
 
 router.post('/reportevent', function (req, res, next) {
-  console.log(req.body);
-  console.log(req.body.data);
-
   const data = req.body.data;
 
   if (!data.hasOwnProperty('apiKey')) { res.status(400).json({ success: false, message: 'Missing device API key' }); }
