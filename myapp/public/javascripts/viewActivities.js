6d0d16a1b1a582b629f95819a2f8e7609cf23673
EvanJWeiler <evanjweiler@gmail.com>
2018-12-10 18:10:13 -0700

    local stuff

--- myapp/public/javascripts/viewActivities.js
+++ myapp/public/javascripts/viewActivities.js
@@ -241,7 +241,7 @@
     for (const activity of activities) {
       console.log('Time: ' + d.getTime());
       console.log('Time of activity: ' + activity.startTime);
-      console.log('Difference: ' + d.getTime() - activity.startTime);
+      console.log('Difference: ' + parseInt(d.getTime() - activity.startTime, 10));
       console.log('1 week time: ' + 7 * 24 * 60 * 60 * 1000);
       console.log(activity.startTime + 7 * 24 * 60 * 60 * 1000 < d.getTime());
       if (activity.startTime + 7 * 24 * 60 * 60 * 1000 < d.getTime()) {
