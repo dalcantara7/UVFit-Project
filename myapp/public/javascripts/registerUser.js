6b37f5c492808d33844998f24117910c503f6417
Jimmy Tran <jimmyt@email.arizona.edu>
2018-12-11 21:59:15 -0700

    register user fix

--- myapp/public/javascripts/registerUser.js
+++ myapp/public/javascripts/registerUser.js
@@ -63,8 +63,8 @@
 
   function validateForm() {
     var flag = true;
-    //$('#email').val() == '^([\w\-\.]+)@((\[([0-9]{1,3}\.){3}[0-9]{1,3}\])|(([\w\-]+\.)+)([a-zA-Z]{2,4}))$'
-    if ($('#email').val().length === 0) {
+
+    if ($('#email').val().length != '^([\w\-\.]+)@((\[([0-9]{1,3}\.){3}[0-9]{1,3}\])|(([\w\-]+\.)+)([a-zA-Z]{2,4}))$') {
       $('#email').addClass('error');
       $('#email-error').css({"display": "block"});
       flag = false;
