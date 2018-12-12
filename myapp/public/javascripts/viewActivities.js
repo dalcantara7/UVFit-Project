de6f22d2fff905a9a676f33c42dbc5b212000332
Jimmy Tran <jimmyt@email.arizona.edu>
2018-12-11 19:56:04 -0700

    syntaax fix

--- myapp/public/javascripts/viewActivities.js
+++ myapp/public/javascripts/viewActivities.js
@@ -422,7 +422,7 @@
 
     document.getElementById('singletable').appendChild(singleTable);
   }
-  
+  /**
   function showForecast(response) {
       navigator.geolocation.getCurrentPosition(function(position) {
           var apiurl = 'api.openweathermap.org/data/2.5/forecast?lat='
@@ -444,4 +444,5 @@
           request.send();
       }
   }
+  **/
 })();
